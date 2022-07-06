3a0674ca2b19643d6531c09dde42f9910f6049ca
Ali Salehi <79578428+Alioo11@users.noreply.github.com>
2022-07-06 21:56:41 +0430

    added posts/id page with ssg

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,13 +1,43 @@
 import React from "react";
-import Link from "next/link";
+import customAxios from "../../custom/customAxios";
+import Head from "next/head";
 
-const Post = () => {
+const Post = ({ data }) => {
   return (
-    <div>
-      hello this is the
-      <Link href="/">hello </Link>
-    </div>
+    <>
+      <Head>
+        <title>{data.title}</title>
+        <meta name="description" content={data.body} />
+      </Head>
+      <div>
+        {data.title}
+        <p>{data.body}</p>
+      </div>
+    </>
   );
 };
+
+export async function getStaticPaths() {
+  const { data } = await customAxios("/posts");
+  const paths = data.map((item) => {
+    return {
+      params: {
+        postId: item.id.toString(),
+      },
+    };
+  });
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const { postId } = params;
+  const { data } = await customAxios(`/posts/${postId}`);
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
 
 export default Post;
